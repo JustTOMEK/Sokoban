f2c6beb8c2a071352889b7ed4b7451d42ad0ee91
tomasz <jubunciak@gmail.com>
2023-01-11 23:26:19 +0100

    Created Tile class

--- tile.py
+++ tile.py
@@ -1,59 +1,49 @@
-class Tile:
+from abc import ABC
+
+
+class Tile(ABC):
     """
     Class tile.
-
-    :param type: type of a tile
-    :type type: string
-
-    :param is_target: if tile is a target
-    :type is_target: bool
     """
-    def __init__(self, char_type):
-        """
-        Initializes tile instance
-        """
-        if not isinstance(char_type, str):
-            raise TypeError("char_type has to be a char")
-        if len(char_type) != 1:
-            raise TypeError("char_type has to be a char")
-        if char_type == "W":
-            raise ValueError("wall can not be a target")
-        self._is_target = char_type.isupper()
-        self._type = self.full_type(char_type.lower())
+    def __init__(self, is_target):
+        if not isinstance(is_target, bool):
+            raise ValueError("is_target has to be a bool")
+        self._is_target = is_target
 
-    def full_type(self, char_type):
-        """
-        Translates char to full tyoe name.
-        """
-        char_to_full = {"f": "floor",
-                        "w": "wall",
-                        "p": "player",
-                        "b": "box"}
-        if char_type not in char_to_full:
-            raise TypeError("Wrong tile char")
-        return char_to_full[char_type]
-
-    def set_type(self, new_type):
-        if new_type not in ["wall", "floor", "player", "box"]:
-            raise ValueError("Wrong new_type")
-        self._type = new_type
-
-    def type(self):
-        return self._type
+    def can_move_into(self):
+        return False
 
     def is_target(self):
         return self._is_target
 
 
-def swap_tiles(tile_1, tile_2):
+class Floor(Tile):
     """
-    Swaps to tiles.
-    is_target stays the same and type changes.
+    Class Floor
     """
-    if not isinstance(tile_1, Tile) or not isinstance(tile_2, Tile):
-        raise TypeError("tiles have to be tile objects")
-    if tile_1.type() == "wall" or tile_2.type() == "wall":
-        raise ValueError("Walls cannot be swapped")
-    tile_1_type = tile_1.type()
-    tile_1.set_type(tile_2.type())
-    tile_2.set_type(tile_1_type)
+    def can_move_into(self):
+        return True
+
+
+class Wall(Tile):
+    """
+    Class Wall.
+    """
+    def can_move_into(self):
+        return False
+
+
+class Player(Tile):
+    """
+    Player tile.
+    """
+    def can_move_into(self):
+        return False
+
+
+class Box(Tile):
+    """
+    Box tile.
+    """
+    def can_move_into(self):
+        return True
